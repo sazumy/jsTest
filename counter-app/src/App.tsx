3aa426e834308d8f173f5c6d5a317c77520be608
sazumy <azumisaito@gmail.com>
2026-02-02 09:37:50 +0900

    init counter-app

---
+++ counter-app/src/App.tsx
@@ -0,0 +1,15 @@
+import Counter from '@/components/Counter.tsx';
+import './App.css';
+
+const title = import.meta.env.VITE_APP_TITLE;
+
+function App() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen space-y-10">
+      <h1>{title}</h1>
+      <Counter />
+    </main>
+  );
+}
+
+export default App;
---
+++ counter-app/src/components/Counter.ng.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    const range = (n: number) => [...Array(n).keys()];
+    range(3).forEach(() => setCount(count + 1));
+    // range(3).forEach(() => setCount((c) => c + 1));
+  }
+
+  function reset() {
+    setCount(0);
+  }
+
+  return (
+    <Card className="min-w-96 gap-2 pt-4 shadow-md">
+      <CardHeader>
+        <div className="text-center text-xl font-medium">Count</div>
+      </CardHeader>
+      <CardContent className="flex justify-center pb-2">
+        <div className="text-4xl font-semibold">{count}</div>
+      </CardContent>
+      <CardContent className="mx-4 flex gap-2">
+        <Button
+          className="flex-1 bg-green-600 hover:bg-green-700"
+          onClick={increment}
+        >
+          +3
+        </Button>
+        <Button className="flex-1 bg-red-600 hover:bg-red-700" onClick={reset}>
+          Reset
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Counter;
